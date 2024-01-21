674dfeb389bdabc2b2e282e8494680627f9591d0
dufrenekm <dufrenekm@gmail.com>
2024-01-21 07:08:46 +0000

    added stopping of car

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -289,6 +289,7 @@ def check_steering_calibration(picar):
         # Loop through, get user input and update value until done
         picar.forward(50)
         time.sleep(1)
+        picar.stop()
         user_in = input("Enter a new value (or e to exit). Old val: %d   : "%float(picar.config_flie.get("picarx_dir_servo", default_value=0)))
         if user_in == 'e':
             break
